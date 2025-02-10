67cc52b836aa0257906421d3cc765e6d19031cb1
AlejandroMontoyaV <alemova.1603@gmail.com>
2025-02-09 20:10:19 -0500

    Navbar organizado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router";
+import "./Navbar.css";
 
 export const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
+      <div className="navbar-components">
+        <div className="left-component">
+            img
+        </div>
+
+        <div className="mid-component">
+            links
+        </div>
+        <div className="right-component">
+            iniciar sesion
+        </div>
+
+      </div>
       <img src="./assets/logo.png" alt="" />
     </nav>
   );
