c5f07c370af80b81db48158c1cff90b7c7812f48
Jcastanour <jcastanour@unal.edu.co>
2025-02-09 20:32:05 -0500

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { BsCart3 } from "react-icons/bs";
+
 import "./Navbar.css";
 
 export const Navbar: React.FC = () => {
@@ -6,16 +9,25 @@ export const Navbar: React.FC = () => {
     <nav className="navbar">
       <div className="navbar-components">
         <div className="left-component">
-            img
+          <Link to="/" className="navbar-link">
+            <img src={logo} alt="logo" className="navbar-logo" />{" "}
+          </Link>
         </div>
 
         <div className="mid-component">
-            links
+          <Link to="/" className="navbar-link">
+            Home
+          </Link>
+          <Link to="/menu" className="navbar-link">
+            Ver Menú
+          </Link>
         </div>
         <div className="right-component">
-            iniciar sesion
+          <Link to="/cart" className="navbar-cart">
+            <BsCart3 />
+          </Link>
+          <button className="navbar-login-button">Inciar sesion</button>
         </div>
-
       </div>
       <img src="./assets/logo.png" alt="" />
     </nav>
