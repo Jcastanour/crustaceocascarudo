39e6bf99fb132311b7722c8ed68cd9fcfbb8087e
Jcastanour <jcastanour@unal.edu.co>
2025-02-09 21:43:00 -0500

    Rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./views/Home";
+import { Menu } from "./views/Menu";
+import { Cart } from "./views/Cart";
+import { Login } from "./views/Login";
 
 export const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Menu" element={<Menu />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
---
+++ src/views/Cart.tsx
@@ -0,0 +1,3 @@
+export const Cart: React.FC = () => {
+  return <h1>Un carrito</h1>;
+};
