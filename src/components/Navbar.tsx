bfe3b4e14ee717bdb397c3600ae5803106654e38
Jcastanour <jcastanour@unal.edu.co>
2025-02-09 19:58:56 -0500

    img

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router";
+import "./Navbar.css";
+import logo from "../assets/logo.png";
 
 export const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <img src="./assets/logo.png" alt="" />
+      <img src={logo} alt="" />
     </nav>
   );
 };
