71ba63f800d1385cfa19c171fe23503797005099
AlejandroMontoyaV <alemova.1603@gmail.com>
2025-03-04 23:44:49 -0500

    mapa

--- Fronted/src/components/Map.tsx
+++ Fronted/src/components/Map.tsx
@@ -1,12 +1,12 @@
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
 
 // Cordenadas de Bikini Atoll, donde se supone esta fondo de Bikini
-const position: [number, number] = [11.5913, 165.3768]; 
+const position: [number, number] = [6.274757055592747, -75.59263764928876]; 
 
 const Map = () => {
   return (
-    <MapContainer center={position} zoom={15} style={{ height: "450px", width: "100%" }}>
+    <MapContainer center={position} zoom={30} style={{ height: "450px", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" // Mapa de OpenStreetMap (gratis y sin API)
       />
